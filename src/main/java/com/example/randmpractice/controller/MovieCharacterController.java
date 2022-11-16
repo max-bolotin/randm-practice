57e43450f7d35765ce0d7bd18e502317928ac181
max-bolotin <realprof.mx@gmail.com>
2022-11-16 16:25:55 +0200

    init commit

---
+++ src/main/java/com/example/randmpractice/controller/MovieCharacterController.java
@@ -0,0 +1,41 @@
+package com.example.randmpractice.controller;
+
+import com.example.randmpractice.model.MovieCharacter;
+import com.example.randmpractice.repository.MovieCharacterRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class MovieCharacterController {
+    private final MovieCharacterRepository repository;
+
+    public MovieCharacterController(MovieCharacterRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World!!";
+    }
+
+    @GetMapping("/all")
+    public List<MovieCharacter> getAll() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public MovieCharacter saveCharacter(/*Long id,
+                                        */String name,
+                                        String status,
+                                        String gender) {
+        MovieCharacter movieCharacter = new MovieCharacter();
+//        movieCharacter.setId(id);
+        movieCharacter.setName(name);
+        movieCharacter.setStatus(status);
+        movieCharacter.setGender(gender);
+        return repository.save(movieCharacter);
+    }
+}
+
